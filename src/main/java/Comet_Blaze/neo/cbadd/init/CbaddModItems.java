d4ee26531471ca520210fd5682cd9ed147792545
ddnjd1 <154037038+ddnjd1@users.noreply.github.com>
2026-05-24 15:46:00 +0800

    Update CbaddModItems.java

--- src/main/java/Comet_Blaze/neo/cbadd/init/CbaddModItems.java
+++ src/main/java/Comet_Blaze/neo/cbadd/init/CbaddModItems.java
@@ -9,13 +9,11 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class CbaddModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CbaddMod.MODID);
-    // 无线红石连接工具
     public static final DeferredItem<Item> CONNECTOR = ITEMS.registerItem("connector", props -> new ConnectorItem(props.stacksTo(1)));
     public static final DeferredItem<BlockItem> KEY_TRIGGER_BLOCK_ITEM =
             ITEMS.register("key_trigger_block",
                     props -> new KeyTriggerBlockItem(CbaddModBlocks.KEY_TRIGGER_BLOCK.get(),
                             new Item.Properties().stacksTo(1)));
-    // 输入/输出方块的物品形式
     public static final DeferredItem<BlockItem> INPUT_BLOCK = ITEMS.registerSimpleBlockItem("input_block", CbaddModBlocks.INPUT_BLOCK);
     public static final DeferredItem<BlockItem> OUTPUT_BLOCK = ITEMS.registerSimpleBlockItem("output_block", CbaddModBlocks.OUTPUT_BLOCK);
     public static final DeferredItem<BlockItem> WIGHTLESS_CONNECTOR_ITEM =
